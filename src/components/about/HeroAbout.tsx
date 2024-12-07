7ab54b85a02f0d07df856c83fea2983b07c5a760
Игорь <2712190@gmail.com>
2024-12-07 19:53:22 +0500

    test

--- src/components/about/HeroAbout.tsx
+++ src/components/about/HeroAbout.tsx
@@ -7,7 +7,7 @@ const HeroAbout = () => {
         <div className="flex flex-col items-center">
           <h1 className="w-full">
             <div className="mb-12">
-              <p className="font-titleAbout font-black text-[286px] font-normal uppercase leading-[215px]">
+              <p className="font-titleAbout text-[286px] uppercase leading-[215px]">
                 you idea
               </p>
             </div>
@@ -17,12 +17,12 @@ const HeroAbout = () => {
                 src="/about/hero-img2.png"
                 alt="Photo"
               />
-              <p className="font-titleAbout font-black text-[286px] font-normal uppercase leading-[215px]">
+              <p className="font-titleAbout text-[286px] uppercase leading-[215px]">
                 our
               </p>
             </div>
             <div className="flex items-center justify-between">
-              <p className="font-titleAbout font-black text-[286px] font-normal uppercase leading-[215px]">
+              <p className="font-titleAbout text-[286px] uppercase leading-[215px]">
                 faces
               </p>
               <img
