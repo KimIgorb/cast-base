b1310afb43a8d879fe900a88394be35b3fbe0d3f
Игорь <2712190@gmail.com>
2024-12-09 23:52:15 +0500

    test

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -70,7 +70,7 @@ const Header: React.FC = () => {
   const logoStyles = isHomeOrClipView ? "text-primary" : "text-secondary";
 
   return (
-    <header className={`fixed top-0 left-0 w-full py-7 z-50 ${headerStyles}`}>
+    <header className={`fixed top-0 left-0 w-full py-3 md:py-5 xl:py-7 z-50 ${headerStyles}`}>
       <Container>
         <div className="flex justify-between items-center">
           <Link to="/" className={`font-title text-4xl mx-auto translate-x-5 md:mx-0 md:translate-x-0 ${logoStyles}`}>
