17849d71facbb4d1e8d08cec7f76c2ec8f06acc1
Игорь <2712190@gmail.com>
2024-12-15 20:03:45 +0500

    test

--- src/components/about/HeroAbout.tsx
+++ src/components/about/HeroAbout.tsx
@@ -2,11 +2,11 @@ import Container from "../Container";
 
 const HeroAbout = () => {
   return (
-    <section className="pt-16 sm:pt-16 md:pt-20 2xl:pt-4">
+    <section className="pt-16 sm:pt-16 md:pt-20">
       <Container>
         <div className="relative flex justify-center items-center">
-          <h1 className="flex justify-center items-center w-full">
-            <img src="/about.png" alt="Your idea our faces" className="xl:h-screen object-contain" />
+          <h1 className="flex justify-center items-center">
+            <img src="/about.png" alt="Your idea our faces" className="object-contain" />
           </h1>
         </div>
       </Container>
