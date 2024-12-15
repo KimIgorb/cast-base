f324ed3a5e1f3b647561ef9eafd8f3fa5b74a239
Игорь <2712190@gmail.com>
2024-12-15 23:55:52 +0500

    test

--- src/components/shared/MainTitle.tsx
+++ src/components/shared/MainTitle.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const MainTitle: React.FC = () => {
 
   return (
-    <h1 className="absolute w-full h-[250px]">
+    <h1>
       <p className="title title_cast">CAST</p>
       <p className="title title_base">BASE</p>
     </h1>
