95bc231a0607538d02bb13a47dd8d6abdd260695
Игорь <2712190@gmail.com>
2024-12-15 23:51:16 +0500

    test

--- src/components/main/Hero.tsx
+++ src/components/main/Hero.tsx
@@ -16,30 +16,33 @@ const Hero: React.FC = () => {
     <section className="bg-secondary pt-16 pb-4">
 
       <Container>
-        <div className="flex justify-center relative">
-          <MainTitle/>
-          <div className="self-end">
-            <img
-              className="max-w-24 mb-2"
-              src="/actors/hero-img2.png"
-              alt="ActorPhoto" />
-            <Text text={heroText.about} className="max-w-32"/>
-            <MainLink text="full" href="/about" isPrimary/>
-          </div>
-          <div className="flex flex-col gap-16">
-            <div className="flex items-end">
+        <div className="flex justify-end relative">
+          <MainTitle />
+          <div className="w-11/12 flex justify-center">
+            <div className="self-end">
               <img
-                className="max-w-16"
-                src="/actors/hero-img1.png"
+                className="max-w-24 mb-2"
+                src="/actors/hero-img2.png"
                 alt="ActorPhoto" />
-              <Text text={heroText.subtitle} className="max-w-12"/>
+              <Text text={heroText.about} className="max-w-32" />
+              <MainLink text="full" href="/about" isPrimary />
             </div>
-            <img
-              className="max-w-20 self-center"
-              src="/actors/hero-img3.png"
-              alt="ActorPhoto" />
+            <div className="flex flex-col items-center gap-12">
+              <div className="flex items-end gap-1">
+                <img
+                  className="max-w-16"
+                  src="/actors/hero-img1.png"
+                  alt="ActorPhoto" />
+                <Text text={heroText.subtitle} className="max-w-12" />
+              </div>
+              <img
+                className="max-w-20"
+                src="/actors/hero-img3.png"
+                alt="ActorPhoto" />
+            </div>
           </div>
         </div>
+
       </Container>
 
     </section>
