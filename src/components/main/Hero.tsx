ae9a49aa21c02810424bc3a4ebe6d1e7cadac6c4
Игорь <2712190@gmail.com>
2024-12-16 17:51:21 +0500

    test

--- src/components/main/Hero.tsx
+++ src/components/main/Hero.tsx
@@ -13,40 +13,40 @@ const heroText = {
 
 const Hero: React.FC = () => {
   return (
-    <section className="relative bg-secondary pt-16 md:pt-24 pb-10">
+    <section className="relative bg-secondary pt-[70px] md:pt-24 xl:pt-28 2xl:pt-32 pb-10">
       <Slogan />
       <Container>
 
-        <div className="flex 2xl:justify-end xl:justify-center lg:justify-center md:justify-center sm:justify-center mb:justify-center justify-center">
-          <div className="flex gap-5 mini:gap-14 justify-center items-end">
+        <div className="flex justify-center">
+          <div className="flex gap-5 mini:gap-14 md:gap-0 justify-center lg:justify-evenly items-end lg:w-full">
             <div className="relative">
               <p className="title title_cast">CAST</p>
               <img
-                className="object-contain 2xl:max-w-[30rem] xl:max-w-80 lg:max-w-80 md:max-w-44 sm:max-w-36 mini:max-w-28 max-w-24 2xl:mb-36 xl:mb-28 lg:mb-20 md:mb-12 sm:mb-12 mb-4"
+                className="object-contain lg:max-w-64 md:max-w-60 sm:max-w-40 mini:max-w-28 max-w-24 mb-4"
                 src="/actors/hero-img2.png"
                 alt="ActorPhoto2"
               />
               <Text
                 text={heroText.about}
-                className="2xl:max-w-lg xl:max-w-lg lg:max-w-96 md:max-w-96 sm:max-w-64 mini:max-w-40 max-w-32"
+                className="2xl:max-w-xl xl:max-w-lg lg:max-w-96 md:max-w-96 sm:max-w-64 mini:max-w-40 max-w-32"
               />
               <MainLink href="/about" text="full" className="block" isPrimary />
             </div>
-            <div className="relative flex flex-col 2xl:gap-36 xl:gap-36 lg:gap-36 md:gap-28 sm:gap-20 mini:gap-12 gap-8 items-center xl:basis-1/3 2xl:basis-auto lg:basis-1/3">
+            <div className="relative flex flex-col  xl:gap-24 sm:gap-20 mini:gap-12 gap-8 items-center">
               <p className="title title_base">BASE</p>
-              <div className="flex items-end">
+              <div className="flex items-end lg:items-center">
                 <img
-                  className="object-contain 2xl:max-w-[21.938rem] xl:max-w-60 w-full lg:max-w-60 md:max-w-44 sm:max-w-36 mini:max-w-24 max-w-20"
+                  className="object-contain  lg:max-w-52 md:max-w-44 sm:max-w-32 mini:max-w-24 max-w-20"
                   src="/actors/hero-img1.png"
                   alt="ActorPhoto1"
                 />
                 <Text
                   text={heroText.subtitle}
-                  className="text-left 2xl:max-w-52 xl:max-w-52 lg:max-w-28 md:max-w-24 sm:max-w-20 max-w-12 2xl:pb-20 xl:pb-8 lg:pb-4 w-full"
+                  className="text-right  xl:max-w-52 lg:max-w-28 md:max-w-32 sm:max-w-20 max-w-12 "
                 />
               </div>
               <img
-                className="object-contain 2xl:max-w-[31.063rem] xl:max-w-80 lg:max-w-60 w-full md:max-w-44 sm:max-w-36 mini:max-w-28 max-w-24"
+                className="object-contain  lg:max-w-60 md:max-w-60 sm:max-w-40 mini:max-w-28 max-w-24"
                 src="/actors/hero-img3.png"
                 alt="ActorPhoto3"
               />
